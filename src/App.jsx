471bd310bd936fd662765d95196792fb79df4507
Pedro Nunes <pedro.nunes@southsystem.com.br>
2023-10-27 13:15:06 -0300

    feat: implemented Header and Sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Pedro Nunes" 
-        content="What an awesome app!!"
-      />
-      <Post 
-        author="Bill Gates"
-        content="We most care for the planet."
-      />
-      <Post />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Pedro Nunes" 
+            content="What an awesome app!!"
+          />
+          <Post 
+            author="Bill Gates"
+            content="We most care for the planet."
+          />
+        </main>
+      </div>
     </div>
   )
 }
