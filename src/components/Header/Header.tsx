846d8ead9a0edacd064c33afd2ea9ea65870e8fa
Pedro Nunes <pedro.nunes@southsystem.com.br>
2023-11-02 07:21:40 -0300

    feat: added header title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ export function Header() {
   return (
     <header className={styles.header}>
       <img src={hatLogo} alt="Cowboy hat logo" />
+      <h3 className={styles.title}>WS Feed</h3>
     </header>
   );
 }
